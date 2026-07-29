36df8582b5610026599cecaf7afbb8d663beeef7
Mi <129257990+m1ch1gan@users.noreply.github.com>
2026-07-29 21:59:51 +0800

    Update Roll_Dice.py

--- Roll_Dice.py
+++ Roll_Dice.py
@@ -1,17 +1,81 @@
+
+dice = {
+    1: (
+        "┌───────┐\n"
+        "│       │\n"
+        "│   ●   │\n"
+        "│       │\n"
+        "└───────┘"
+    ),
+    2: (
+        "┌───────┐\n"
+        "│ ●     │\n"
+        "│       │\n"
+        "│     ● │\n"
+        "└───────┘"
+    ),
+    3: (
+        "┌───────┐\n"
+        "│ ●     │\n"
+        "│   ●   │\n"
+        "│     ● │\n"
+        "└───────┘"
+    ),
+    4: (
+        "┌───────┐\n"
+        "│ ●   ● │\n"
+        "│       │\n"
+        "│ ●   ● │\n"
+        "└───────┘"
+    ),
+    5: (
+        "┌───────┐\n"
+        "│ ●   ● │\n"
+        "│   ●   │\n"
+        "│ ●   ● │\n"
+        "└───────┘"
+    ),
+    6: (
+        "┌───────┐\n"
+        "│ ●   ● │\n"
+        "│ ●   ● │\n"
+        "│ ●   ● │\n"
+        "└───────┘"
+    ),
+}
+
+
 #import random
 import random
+
+#define Roll dice number
+def Roll_Dice():
+    return random.randint(1,6)
+
 #Prompts User to select Y/N
-RollDice = input("Roll the dice? (y/n): ")
-UserResponse = RollDice.upper
-#Logic for when user responds 
-if UserResponse == str("Y").upper:
-    RollDiceNumber1 =random.randint(0, 100)
-    RollDiceNumber2 =random.randint(0,100)
-    print(f"(",str(RollDiceNumber1) + ", " + str(RollDiceNumber2),f")")
-elif UserResponse == str("N").upper:
-    pass
+Response = input("Roll the dice? (y/n): ").lower()
+
+#With "Y" response and roll dice, output corresponding ASCII art
+if Response == "y":
+    Roll_Dice()
+    result = Roll_Dice()
+    if result == 1:
+        print(dice[1])
+    elif result == 2:
+        print(dice[2])
+    elif result == 3:
+        print(dice[3])
+    elif result == 4:
+        print(dice[4])
+    elif result == 5:
+        print(dice[5])
+    else:
+        print(dice[6])
+elif Response == "n":
     print("Thanks for playing!")
+    pass      
 else:
-    print("Invalid Choice!")
+    pass
+    print("Invalid answer!")
 
 
